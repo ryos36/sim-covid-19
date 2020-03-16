52aac5153d201f5851d9f1a4bcc24f633d828482
Ryos Suzuki <ryos@sinby.com>
2020-03-16 19:25:52 +0900

    パラメタを増やす

--- covid19.py
+++ covid19.py
@@ -4,62 +4,147 @@ width=1920
 height=1080
 earth=[[0]*height for i in range(width)]
 #print(len(earth[0]))
-earth[int(width*random.random())][int(height*random.random())] = 2 
+w0=500
+h0=500
+w0=int(width*random.random())
+h0=int(height*random.random())
+earth[w0][h0] = 1 
 #print(earth[1079][1919])
 r0=1.8
-days0=1
-days1=14
 move_n=int(width*height * 0.1)
-days=days0+days1
 around=8
 pos = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, -1), (-1, 1), (0, 1), (1, 1)]
+
+STATE0_INIT=0
+STATE1_INFECTION=0x100
+STATE2_SPREADER=0x200
+STATE3_SERIOUS=0x300
+STATE4_DEAD=0x400
+STATE5_REVIVE=0x500
+STATE6_BLOCKER=0x600
+STATE7_NO_PERSON=0x700
+STATE8_MOLE=0x800
+
+spreader_rate=0.5
+days0=1
+days1=14
 rate=r0/(days1 * around)
-print(rate)
+serious_rate=0.1/days1
+serious_days=10
+dead_rate=0.1/serious_days
+revive_days=10
+STATE0_MARKED=days0 + 1
+
+def next_state(state_days):
+    state = state_days & 0xFF00
+    days = state_days & 0xFF
+
+    new_state_days = state
+    if state_days < 0:
+        pass
+    elif state_days == STATE0_MARKED:
+        pass
+    elif state == STATE0_INIT:
+        if days == 0:
+            pass
+        elif days == days0:
+            if random.random() < spreader_rate:
+                new_state_days = STATE1_INFECTION
+            else:
+                new_state_days = STATE2_SPREADER
+        else:
+            days += 1
+
+    elif (state == STATE1_INFECTION) or (state == STATE2_SPREADER):
+        if days == days1:
+            new_state_days = STATE6_BLOCKER
+        else:
+            if random.random() < serious_rate:
+                new_state_days = STATE3_SERIOUS
+            else:
+                days += 1
+    elif state == STATE3_SERIOUS:
+        if days == serious_days:
+            new_state_days = STATE5_REVIVE
+        else:
+            if random.random() < serious_rate:
+                new_state_days = STATE4_DEAD
+            else:
+                days += 1
+    elif state == STATE5_REVIVE:
+        if days == revive_days:
+            new_state_days = STATE6_BLOCKER
+        else:
+            days += 1
+
+    if state == new_state_days:
+        return state + days
+    else:
+        return new_state_days
 
 now_day = 1
+dead_n = 0
 while True:
+    state_n = [0] * 8
+
     for w in range(width):
         for h in range(height):
             v = earth[w][h]
-            if (0 < v) and (v <= days):
-                earth[w][h] += 1
-                if ( v <= days0 ):
-                    continue
+            earth[w][h] = next_state(v)
+            state = v & 0xFF00
+
+            if (state == STATE1_INFECTION) or (state == STATE2_SPREADER):
+            
                 for p in pos:
                     target_p = tuple(map(lambda x,y:x+y, (w, h), p))
                     if ((target_p[0] < 0) or (width <= target_p[0]) or
                         (target_p[1] < 0) or (height <= target_p[1])):
                         continue
 
-                    if earth[target_p[0]][target_p[1]] != 0 :
+                    if earth[target_p[0]][target_p[1]] != STATE0_INIT :
                         continue
 
                     r = random.random()
                     hit = (r < rate)
                     #print(r, rate, hit)
                     if hit:
-                        earth[target_p[0]][target_p[1]] = 1
+                        earth[target_p[0]][target_p[1]] = STATE0_MARKED
 
-    n = 0
-    done_n = 0
     for w in range(width):
         for h in range(height):
             v = earth[w][h]
-            if (0 < v) and (v <= days):
-                n += 1
-                #print(w, h, earth[w][h])
-            elif ( days < v ):
-                done_n += 1
-    print(now_day, n, done_n, flush=True)
+            if v >= STATE7_NO_PERSON:
+                continue;
+            if v == STATE0_MARKED:
+                earth[w][h] = 1
+            state = v & 0xFF00
+            if state == STATE0_INIT:
+                if v == STATE0_INIT:
+                    state_n[0] += 1
+                else:
+                    state_n[7] += 1
+            else:
+                state_n[state >> 8] += 1
+
+            if state == STATE4_DEAD:
+                dead_n += 1
+                earth[w][h] = STATE7_NO_PERSON
+
+            #if ( v > 0 ) and ( v < 100):
+            #    print('here', w, h, v, earth[w][h]);
+
+    print(now_day, state_n, dead_n, flush=True)
     now_day += 1
-    if n == 0:
-        print(done_n, done_n / (width * height))
+    if (state_n[1] == 0) and (state_n[2] == 0) and (state_n[3] == 0) and (state_n[5] == 0) and (state_n[7] == 0):
+        print(state_n, state_n[6] / (width * height), dead_n)
         break
     for i in range(move_n):
         x0 = int(width*random.random())
         y0 = int(height*random.random())
         x1 = int(width*random.random())
         y1 = int(height*random.random())
-        earth[x0][y0], earth[x1][y1] = earth[x1][y1], earth[x0][y0]
-
+        v0 = earth[x0][y0]
+        v1 = earth[x1][y1]
+        if ( v0 != STATE8_MOLE ) and ( v1 != STATE8_MOLE ) and ( v0 != STATE1_INFECTION ) and ( v0 != STATE1_INFECTION ) and ( v0 != STATE3_SERIOUS ) and ( v1 != STATE3_SERIOUS) and ( v0 != STATE5_REVIVE) and ( v1 != STATE5_REVIVE ) :
+            earth[x0][y0], earth[x1][y1] = v1, v0
 
